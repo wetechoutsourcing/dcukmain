2cf34df5c52ce8b5db430e1bf21becb2d72c5995
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-07-10 14:01:33 +0530

    Refactor product card list to display 4 products instead of 3

--- apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -34,7 +34,7 @@ export default async function ProductRail({
           View all
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
+      <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-6 gap-y-24 small:gap-y-36">
         {pricedProducts &&
           pricedProducts.map((product) => (
             <li key={product.id}>
